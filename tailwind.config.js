ee03af1861260caad6595749b95f611027adf792
francojnieva <franco97nieva@hotmail.com>
2023-11-15 19:24:49 -0300

    :art: add banner, form and footer. install iconify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    addDynamicIconSelectors()
+  ],
 }
 
